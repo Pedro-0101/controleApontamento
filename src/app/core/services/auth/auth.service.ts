160a05f9c5bc3d290bd5a5c10f2a946c2c8980d8
Pedro-0101 <pedro.tiete@outlook.com>
2025-12-09 16:20:40 -0300

    feat: criado service para login

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { LoggerService } from '../logger/logger.service';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  
+
+  private logger = inject(LoggerService);
+  private users = environment.dnpAccessCode;
+  userName = signal('');
+
 }
